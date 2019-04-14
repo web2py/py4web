4dc0277acfcc34b41b26ddea9948a4ba2f6a874e
mdipierro <massimo.dipierro@gmail.com>
2019-04-14 16:02:02 -0700

    test action functional

--- web3py/tests/test_action.py
+++ web3py/tests/test_action.py
@@ -1,16 +1,48 @@
+import multiprocessing
+import os
+import time
 import unittest
+import uuid
 
-from web3py import action
+import mechanize
 
-os.environ['WEB2PY_APPLICATIIONS'] = 'applications'
+from web3py import action, DAL, Field, Session, Cache
+from web3py.core import bottle
 
+os.environ['WEB3PY_APPLICATIONS'] = 'applications'
+
+db = DAL('sqlite://storage_%s' % uuid.uuid4(), folder='/tmp/')
+db.define_table('thing', Field('name'))
+session = Session(secret='my secret')
+cache = Cache()
 
 @action('/index')
+@cache.memoize(expiration=1)
+@action.uses(db, session)
 def index():
-    return 'ok'
-
+    db.thing.insert(name='test')
+    session['number'] = session.get('number', 0) + 1
+    return 'ok %s %s' % (session['number'], db(db.thing).count())
 
 class CacheAction(unittest.TestCase):
 
+    def setUp(self):
+        self.server = multiprocessing.Process(target=lambda: bottle.run(host='localhost', port=8001))
+        self.server.start()
+        self.browser = mechanize.Browser()
+        time.sleep(1)
+
+    def tearDown(self):
+        self.server.terminate()
+
     def test_action(self):
-        pass
+        res = self.browser.open('http://127.0.0.1:8001/index')
+        self.assertEqual(res.read(), b'ok 1 1')
+
+        res = self.browser.open('http://127.0.0.1:8001/index')
+        self.assertEqual(res.read(), b'ok 1 1')
+
+        time.sleep(2)
+        
+        res = self.browser.open('http://127.0.0.1:8001/index')
+        self.assertEqual(res.read(), b'ok 2 2')
