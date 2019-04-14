800477f8b2470239764de8b0d703581600437ffe
mdipierro <massimo.dipierro@gmail.com>
2019-04-14 15:26:35 -0700

    moved tests

--- web3py/tests/test_action.py
+++ web3py/tests/test_action.py
@@ -1,6 +1,13 @@
 import unittest
 
-import web3py
+from web3py import action
+
+os.environ['WEB2PY_APPLICATIIONS'] = 'applications'
+
+
+@action('/index')
+def index():
+    return 'ok'
 
 
 class CacheAction(unittest.TestCase):
