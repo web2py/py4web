08222d89905a33ff864e3992b0a330a30383d2b7
mdipierro <massimo.dipierro@gmail.com>
2019-04-26 23:50:16 -0700

    new version

---
+++ web3py/tests/test_url.py
@@ -0,0 +1,16 @@
+import random
+import time
+import unittest
+
+from web3py import request, URL
+
+
+class TestURL(unittest.TestCase):
+
+    def test_url(self):
+        request.app_name = '_default'
+        self.assertEqual(URL('index'), '/index')
+        request.app_name = 'app'
+        self.assertEqual(URL('index'), '/app/index')
+        self.assertEqual(URL('a','b',vars=dict(x=1),hash='y'), '/app/a/b?x=1#y')
+
