a53321192835ddaf8b97cc92d09910fb491d013e
mdipierro <massimo.dipierro@gmail.com>
2019-07-23 22:31:17 -0700

    some cleanup

---
+++ apps/_default/__init__.py
@@ -0,0 +1,6 @@
+from py4web import action, __version__
+
+@action('index')
+@action.uses('index.html')
+def index():
+    return dict(version=__version__)
---
+++ apps/_minimal/__init__.py
@@ -0,0 +1,5 @@
+from py4web import action
+
+@action('index')
+def index():
+    return "Hello World"
