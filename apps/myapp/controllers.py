e5e70c72492992a42bc8bd9f1581973598573800
mdipierro <massimo.dipierro@gmail.com>
2019-07-23 22:29:48 -0700

    some cleanup

--- apps/myapp/controllers.py
+++
@@ -1,6 +0,0 @@
-from py4web import action, request, redirect, abort
-from . models import db
-
-@action('helloworld') # exposes http://127.0.0.1:8000/myapp/helloworld
-@action.uses(db, 'helloworld.html')
-def helloworld(): return dict(name=request.forms.get('name', 'visitor'))
--- apps/myapp/models.py
+++
@@ -1,4 +0,0 @@
-import os
-from py4web import DAL, Field
-db = DAL('sqlite://test', folder=os.path.join(os.path.dirname(__file__), 'databases'))
-db.define_table('thing', Field('name'))
