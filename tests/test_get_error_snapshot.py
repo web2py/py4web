b810a1f8ca07e8701418d55ad18f3493f6ba88e3
mdipierro <massimo.dipierro@gmail.com>
2019-08-27 23:15:55 -0700

    core code coverage 81%

---
+++ tests/test_get_error_snapshot.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+from py4web.core import get_error_snapshot
+
+
+class ErrorTest(unittest.TestCase):
+
+    def test_get_error_snapshot(self):
+        try:
+            1/0
+        except Exception:
+            snapshot = get_error_snapshot()
+            keys = list(sorted(snapshot.keys()))
+        self.assertEqual(keys, ['exception_type',
+                                'exception_value',
+                                'os_environ',
+                                'platform_info',
+                                'python_version',
+                                'stackframes',
+                                'timestamp',
+                                'traceback'])
---
+++ tests/test_main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import unittest
+import tempfile
+import time
+import signal
+from multiprocessing import Process
+from unittest.mock import patch
+from py4web.core import main
+
+def patched_main():
+    dirpath = tempfile.mkdtemp()
+    dir = os.path.join(dirpath, 'apps')
+    testargs = ["py4web", "-d", "demo", "-c", dir]
+    with patch.object(sys, 'argv', testargs):
+        main()
+
+class MainTest(unittest.TestCase):
+
+    def test_main(self):
+        class MyException(Exception): pass
+        def handler(signum, frame):
+            raise MyException
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(10)
+        try:
+            patched_main()
+        except MyException:
+            pass
