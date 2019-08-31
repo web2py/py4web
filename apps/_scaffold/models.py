36f197cddab7ec590615a3238450977688b4c3ad
Micah Beasley <MBfromOK@users.noreply.github.com>
2019-08-31 18:18:20 -0500

    3 adds, 1 deletion: see details
    Add missing __init__py for /apps/
    Add code to check for /apps/app/databases folder and create if missing.
    Add missing assets folder (compared to pip version)
    remove databases file that was a bad fix to missing folder issue

--- apps/_scaffold/models.py
+++ apps/_scaffold/models.py
@@ -4,6 +4,13 @@ This file defines the database connection(s) and models
 import os
 from py4web import DAL, Field
 from . import settings
+
+if not os.path.exists(settings.DB_FOLDER):
+    try:
+        os.makedirs(settings.DB_FOLDER)
+    except FileExistsError:
+        pass
+
 # the DAL object (does not have to be called db but it is a useful convention)
 db = DAL(settings.DB_URI, folder=settings.DB_FOLDER, pool_size=settings.DB_POOL_SIZE)
 
