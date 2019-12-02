281cf2042ba7f7d8e08844e1490aa52e2b1f46d0
Luca de Alfaro <luca@dealfaro.org>
2019-12-01 17:34:43 -0800

    Work on form validation

--- apps/_documentation/static/chapters/en/chapter-10.mm
+++ apps/_documentation/static/chapters/en/chapter-10.mm
@@ -2,12 +2,79 @@ WORK IN PROGRESS
 
 Just know that ``py4web.utils.form.Form`` is a pretty much equivalent to web2py's ``SQLFORM``.
 
-Form will accept the following:
-    - table: a DAL table or a list of fields (equivalent to old SQLFORM.factory)
-    - record: a DAL record or record id
-    - readonly: set to True to make a readonly form
-    - deletable: set to False to disallow deletion of record
-    - formstyle: a function that renders the form using helpers (FormStyleDefault)
-    - dbio: set to False to prevent any DB writes
-    - keep_values: if set to true, it remembers the values of the previously submitted form
-    - form_name: the optional name of this form
+The `Form` constructor accepts the following arguments:
+
+``
+Form(self,
+     table,
+     record=None,
+     readonly=False,
+     deletable=True,
+     formstyle=FormStyleDefault,
+     dbio=True,
+     keep_values=False,
+     form_name=False,
+     hidden=None,
+     before_validate=None):
+``:python
+
+Where:
+
+- `table`: a DAL table or a list of fields (equivalent to old SQLFORM.factory)
+- `record`: a DAL record or record id
+- `readonly`: set to True to make a readonly form
+- `deletable`: set to False to disallow deletion of record
+- `formstyle`: a function that renders the form using helpers (FormStyleDefault)
+- `dbio`: set to False to prevent any DB writes
+- `keep_values`: if set to true, it remembers the values of the previously submitted form
+- `form_name`: the optional name of this form
+- `hidden`: a dictionary of hidden fields that is added to the form
+- `before_validate`: an optional validator.
+
+## Example
+
+Here is a simple example of a custom form not using database access.
+We declare an endpoint `/form_example`, which will be used both for the GET and for the POST of the form:
+
+``
+@action('form_example', method=['GET', 'POST'])
+@action.uses('form_example.html', session)
+def form_example():
+    form = Form([
+        Field('product_name'),
+        Field('product_quantity', 'integer')],
+        formstyle=FormStyleBulma)
+    if form.accepted:
+        # Do something with form.vars['product_name'] and form.vars['product_quantity']
+        redirect(URL('index'))
+    return dict(form=form)
+``:python
+
+The form can be displayed in the template simply using `[[=form]]`.
+
+## Form validation
+
+The validation of form input can be done in two ways.  One can define `requires` attributes of `Field`, or one can define explicitly a validation function.  To do the latter, we pass to `validate` a function that takes the form and returns a dictionary, mapping field names to errors.  If the dictionary is non-empty, the errors will be displayed to the user, and no database I/O will take place.
+
+Here is an example:
+
+``
+def check_nonnegative_quantity(form):
+    errors = {}
+    if form.vars['product_quantity'] < 0:
+        errors['product_quantity'] = T('The product quantity cannot be negative')
+    return errors
+
+@action('form_example', method=['GET', 'POST'])
+@action.uses('form_example.html', session)
+def form_example():
+    form = Form([
+        Field('product_name'),
+        Field('product_quantity', 'integer')],
+        validation=check_nonnegative_quantity,
+        formstyle=FormStyleBulma)
+    if form.accepted:
+        # Do something with form.vars['product_name'] and form.vars['product_quantity']
+        redirect(URL('index'))
+    return dict(form=form)
+``:python
