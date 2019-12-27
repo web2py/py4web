abe75126d556350d91fd5eaac23db0af6e3529a2
mdipierro <massimo.dipierro@gmail.com>
2019-12-27 18:29:04 +0100

    removed unwanted file

--- trunk/bottle_app.py
+++
@@ -1,7 +0,0 @@
-import os
-from py4web.core import wsgi
-password_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'password.txt'))
-application = wsgi(password_file=password_file, dashboard_mode='full', apps_folder="mysite/apps")
-
-
-test763894
