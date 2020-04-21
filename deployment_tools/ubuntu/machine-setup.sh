210403d4a4d8a99676c68480de5ae33d4b65595b
mdipierro <massimo.dipierro@gmail.com>
2020-04-20 21:30:11 -0700

    deployment_tools/ubuntu/machine-setup.sh

---
+++ deployment_tools/ubuntu/machine-setup.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+echo "======================================="
+echo "Configuring IPv6 firewall"
+echo "======================================="
+ip6tables -P INPUT DROP
+ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+echo "Starting IPv4 firewall"
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+ 
+#unlimited 
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+ 
+# DROP all incomming traffic
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+ 
+# Block sync
+iptables -A INPUT -p tcp ! --syn -m state --state NEW  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+ 
+# Block Fragments
+iptables -A INPUT -f  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
+iptables -A INPUT -f -j DROP
+ 
+# Block bad stuff
+iptables  -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+iptables  -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+ 
+iptables  -A INPUT -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
+iptables  -A INPUT -p tcp --tcp-flags ALL NONE -j DROP # NULL packets
+ 
+iptables  -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+ 
+iptables  -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "XMAS Packets"
+iptables  -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP #XMAS
+ 
+iptables  -A INPUT -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fin Packets Scan"
+iptables  -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP # FIN packet scans
+ 
+iptables  -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+ 
+# Allow full outgoing connection but no incomming stuff
+iptables -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o eth0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+ 
+# Allow ssh 
+iptables -A INPUT -p tcp --destination-port 22 -j ACCEPT
+ 
+# allow incomming ICMP ping pong stuff
+iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+# Allow port 53 tcp/udp (DNS Server)
+iptables -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -p udp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+iptables -A INPUT -p tcp --destination-port 53 -m state --state NEW,ESTABLISHED,RELATED  -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+# Open port 80
+iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 443 -j ACCEPT
+##### Add your rules below ######
+ 
+##### END your rules ############
+
+# log everything else and drop
+iptables -A INPUT -j LOG
+iptables -A FORWARD -j LOG
+iptables -A INPUT -j DROP
+
+echo "======================================="
+echo "Installing Packages"
+echo "======================================="
+apt-get update
+apt-get -y install emacs
+apt-get -y install zip unzip
+apt-get -y install tar
+apt-get -y install postgresql
+apt-get -y install libpq-dev
+apt-get -y install sendmail
+apt-get -y install fail2ban
+apt-get -y install supervisor
+apt-get -y install nginx
+apt-get -y install python3.7
+apt-get -y install python3.7-pip
+apt-get -y install python3.7-dev
+apt-get -y install postgresql-client
+apt-get -y install postgresql-client-common
+apt-get -y install sendmail
+apt-get -y install redis-server
+
+echo "======================================="
+echo "Installing Python Packages for py4web"
+echo "======================================="
+cat > requirements-py4web.txt <<EOF
+tornado
+gevent
+gunicorn
+requests
+redis
+psycopg2
+py4web
+EOF
+
+python3.7 -m pip install -r requirements-py4web.txt
+
+if [ -f requirements.txt ]
+then
+    echo "======================================="
+    echo "Installing Packages for apps"
+    echo "======================================="
+    python3.7 -m pip install -r requirements.txt
+fi
+
+echo "======================================="
+echo "configuring database"
+echo "======================================="
+sudo -u postgres createuser default -P --interactive
+sudo -u postgres createdb default
+
+if [ ! -f /home/www-data/py4web ]
+then
+    echo "======================================="
+    echo "creading missing folder"
+    echo "======================================="
+    mkdir -p /home/www-data/py4web
+    chown -R www-data:www-data /home/www-data/py4web/apps
+fi
+
+if [ ! -d /etc/nginx/sites-available/py4web ]
+then
+    echo "======================================="
+    echo "configuring NGINX"
+    echo "======================================="
+    mkdir -p /etc/nginx/conf.d/py4web
+# Create configuration file /etc/nginx/sites-available/py4web
+echo 'server {
+        listen          80;
+        server_name     $hostname;
+        location ~* ^/(\w+)/static(?:/_[\d]+\.[\d]+\.[\d]+)?/(.*)$ {
+            alias /home/www-data/py4web/apps/$1/static/$2;
+            expires max;
+        }
+        location / {
+            proxy_pass      http://127.0.0.1:8000;
+        }
+}
+server {
+        listen 443 default_server ssl;
+        server_name     $hostname;
+        ssl_certificate         /etc/nginx/ssl/py4web.crt;
+        ssl_certificate_key     /etc/nginx/ssl/py4web.key;
+        ssl_prefer_server_ciphers on;
+        ssl_session_cache shared:SSL:10m;
+        ssl_session_timeout 10m;
+        ssl_ciphers ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA:DHE-DSS-AES256-SHA:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA;
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+        keepalive_timeout    70;
+        location / {
+            proxy_pass      http://127.0.0.1:8000;
+        }
+        location ~* ^/(\w+)/static(?:/_[\d]+\.[\d]+\.[\d]+)?/(.*)$ {
+            alias /home/www-data/py4web/apps/$1/static/$2;
+            expires max;
+        }
+}' >/etc/nginx/sites-available/py4web
+ln -s /etc/nginx/sites-available/py4web /etc/nginx/sites-enabled/py4web
+rm /etc/nginx/sites-enabled/default
+fi
+
+if [ ! -f /etc/nginx/ssl/py4web.crt ]
+then
+    echo "======================================="
+    echo "creating a self signed certificate"
+    echo "======================================="
+    mkdir -p /etc/nginx/ssl
+    pushd /etc/nginx/ssl
+    openssl genrsa 1024 > py4web.key
+    chmod 400 py4web.key
+    openssl req -new -x509 -nodes -sha1 -days 1780 -key py4web.key > py4web.crt
+    openssl x509 -noout -fingerprint -text < py4web.crt > py4web.info
+    popd
+fi
+
+if [ ! -f /etc/init.d/py4web ]
+   then:
+
+echo '
+#! /bin/sh
+
+NAME=py4web
+DESC="py4web process"
+PIDFILE="/var/run/${NAME}.pid"
+LOGFILE="/var/log/${NAME}.log"
+DAEMON="/usr/local/bin/py4web-start"
+DAEMON_OPTS="--headless --password_file /home/www-data/py4web/password.txt /home/www-data/py4web/apps"
+START_OPTS="--start --background --make-pidfile --pidfile ${PIDFILE} --exec ${DAEMON} -- ${DAEMON_OPTS}"
+STOP_OPTS="--stop --pidfile ${PIDFILE}"
+
+test -x $DAEMON || exit 0
+set -e
+
+case "$1" in
+start)
+  echo -n "Starting ${DESC}: "
+  start-stop-daemon $START_OPTS >> $LOGFILE
+  echo "$NAME."
+  ;;
+stop)
+  echo -n "Stopping $DESC: "
+  start-stop-daemon $STOP_OPTS
+  echo "$NAME."
+  rm -f $PIDFILE
+  ;;
+restart|force-reload)
+  echo -n "Restarting $DESC: "
+  start-stop-daemon $STOP_OPTS
+  sleep 1
+  start-stop-daemon $START_OPTS >> $LOGFILE
+  echo "$NAME."
+  ;;
+*)
+  N=/etc/init.d/$NAME
+  echo "Usage: $N {start|stop|restart|force-reload}" >&2
+  exit 1
+  ;;
+esac
+exit 0
+' > /etc/init.d/py4web
+
+/etc/init.d/py4web restart
+/etc/init.d/nginx restart
