323addcf336adaf048417bbfa7ab7e91816c0b71
Jim Steil <jim@steilonline.com>
2020-08-21 13:19:19 -0500

    Multi-select list in FORM only returns last value
    Handle multiple values coming back from the bottle request for 'list' type fields.  Reference https://stackoverflow.com/questions/22579797/how-to-get-all-items-in-multi-list-in-bottle
    
    This change uses getall and then returns the first value if length of the list == 1 else returns the whole list.

--- py4web/utils/form.py
+++ py4web/utils/form.py
@@ -368,7 +368,8 @@ class Form(object):
             if self.record:
                 self.vars = self._read_vars_from_record(table)
         else:
-            post_vars = self.vars = request.forms
+            original_value = post_vars.getall(field.name)
+            original_value = original_value[0] if len(original_value) == 1 else original_value
             self.submitted = True
             process = False
 
