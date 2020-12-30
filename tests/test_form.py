47c3062705c2401c3b5416f13bc3bba205366dfd
Massimo Di Pierro <massimo.dipierro@gmail.com>
2020-12-30 15:10:23 -0800

    fixed cookie spilling in tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,10 +1,16 @@
+import io
 import os
 import unittest
-from py4web import response, Field
+from py4web import request, response, Field
 from py4web.utils.form import Form
 
 
 class FormTest(unittest.TestCase):
+    def setUp(self):
+        request.environ['wsgi.input'] = io.BytesIO()
+        request.cookies.clear()
+        response._cookies = ""
+
     def test_form(self):
         table = [Field("name")]
         form_name = 'testing_form'
