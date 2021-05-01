5dfcb3410175c910a32446eff2803792f9d2a7d0
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-05-01 09:43:40 -0700

    moved websocket and no more need for jquery

--- apps/examples/socketio/__init__.py
+++ apps/examples/socketio/__init__.py
@@ -5,12 +5,12 @@ from py4web import action, request
 # pls, run socketio server - look at utils/wsservers.py
 # test example for python-socketio
 
-@action("index")
-@action.uses("index.html")
+@action("socketio/index")
+@action.uses("socketio/index.html")
 def index():
     return dict()
 
-@action('echo/<path:path>', method=["GET", "POST"])
+@action('socketio/echo/<path:path>', method=["GET", "POST"])
 def echo(path=None):
     print (path)
     print ('GET from sio-server')
--- apps/examples/ws/__init__.py
+++ apps/examples/ws/__init__.py
@@ -4,8 +4,8 @@ from py4web import action, request
 # pls, run websockets server - look at utils/wsservers.py
 # test example for websockets
 
-@action("index")
-@action.uses("index.html")
+@action("ws/index")
+@action.uses("ws/index.html")
 def index():
     return dict()
 
