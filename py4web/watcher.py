3fb9defe34e75d52de8af33fb7d0a12a2170f2a6
unknown <valq7711@gmail.com>
2021-05-08 01:50:26 +0300

    add watcher

---
+++ py4web/watcher.py
@@ -0,0 +1,61 @@
+import urllib3
+import json
+import asyncio
+from watchgod import awatch
+import os
+import click
+
+
+http = urllib3.PoolManager()
+data = {'args': ['changes']}
+encoded_data = json.dumps(data).encode('utf-8')
+
+
+apps_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../apps'))
+
+
+class Params:
+    host_port = None
+    cli_key = None
+
+
+def watch(apps_folder, mode="sync"):
+    def watch_folder_event_loop(apps_folder):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(watch_folder(apps_folder))
+
+    async def watch_folder(apps_folder):
+        async for changes in awatch(os.path.join(apps_folder)):
+            notify(changes)
+
+    watch_folder_event_loop(apps_folder)
+
+
+def notify(changes):
+    changes = dict(args = [list(changes)])
+    changes = json.dumps(changes).encode('utf-8')
+    http.request(
+        'POST',
+        f'http://{Params.host_port}/py4web_cli/files_changed',
+        body = changes,
+        headers={
+            'Content-Type': 'application/json',
+            'X-Py4web-Cli-Key': Params.cli_key
+        }
+    )
+
+
+@click.command()
+@click.option("-H", "--host", default="127.0.0.1", help="Host name", show_default=True)
+@click.option(
+    "-P", "--port", default=8000, type=int, help="Port number", show_default=True
+)
+@click.argument('cli_key')
+def run(host, port, cli_key):
+    Params.host_port = f'{host}:{port}'
+    Params.cli_key = cli_key
+    watch(apps_folder)
+
+if __name__ == '__main__':
+    run()
