8c46a656f9f705221cd6962cdf3e0e7e8cc6f020
Andrew Gavgavian <andrewgee7@gmail.com>
2021-06-28 22:56:37 -0700

    Adjusted mtable to work with booleans and date. Updated warning to not be depricated

--- apps/_dashboard/static/components/mtable.js
+++ apps/_dashboard/static/components/mtable.js
@@ -1,71 +1,73 @@
-(function(){
+(function () {
 
-    var mtable = { props: ['url', 'filter', 'order', 'editable', 'create', 'deletable', 'render'], data: null, methods: {}};
-    
-    mtable.data = function() {        
-        var data = {url: this.url,
-                    busy: false,
-                    filter: this.filter || '',
-                    order: this.order ||  '',
-                    errors: {},
-                    item: null,
-                    message: '',
-                    reference_options: {},
-                    table: { model: [], items: [], count: 0}};
+    var mtable = { props: ['url', 'filter', 'order', 'editable', 'create', 'deletable', 'render'], data: null, methods: {} };
+
+    mtable.data = function () {
+        var data = {
+            url: this.url,
+            busy: false,
+            filter: this.filter || '',
+            order: this.order || '',
+            errors: {},
+            item: null,
+            message: '',
+            reference_options: {},
+            table: { model: [], items: [], count: 0 }
+        };
         mtable.methods.load.call(data);
         return data;
     };
-    
-    mtable.methods.toggle = function(obj, key) {
+
+    mtable.methods.toggle = function (obj, key) {
         obj[key] = !obj[key];
     };
-    mtable.methods.load = function() {
+    mtable.methods.load = function () {
         let self = this;
         let length = this.table.items.length;
         let url = this.url + '?@limit=20';
-        if (length) url+='&@offset='+length; else url+='&@model=true';
-        let filters = self.filter.split(' and ').filter((f)=>{return f.trim() != ''});
-        filters = filters.filter((f)=>{return f.trim();}).map((f)=>{                
-                let parts = (f
-                             .replace(/ equals? /,'==')
-                             .replace('==','.eq=')
-                             .replace('!=','.ne=')
-                             .replace('<','.lt=')
-                             .replace('>','.gt=')
-                             .replace('<=','.le=')
-                             .replace('>=','.ge=')
-                             .replace(' in ','.in=')
-                             .replace(' startswith ','~')
-                             .replace('~','startswith=')
-                             .split(/[=]/));
-                let negate = false; 
-                if (parts[0].substr(0,4) == 'not ') {
-                    negate=true; 
-                    parts[0]=parts[0].substr(4);
-                }
-                return ((negate?'not.':'') + 
-                        parts[0].replace(/ /ig,'') +
-                        '=' + parts[parts.length-1].replace(/^ /,''));
-            });
-        if (filters.length) url += '&'+filters.join('&');
-        if (self.order) url += '&@order='+self.order;
+        if (length) url += '&@offset=' + length; else url += '&@model=true';
+        let filters = self.filter.split(' and ').filter((f) => { return f.trim() != '' });
+        filters = filters.filter((f) => { return f.trim(); }).map((f) => {
+            let parts = (f
+                .replace(/ equals? /, '==')
+                .replace('==', '.eq=')
+                .replace('!=', '.ne=')
+                .replace('<', '.lt=')
+                .replace('>', '.gt=')
+                .replace('<=', '.le=')
+                .replace('>=', '.ge=')
+                .replace(' in ', '.in=')
+                .replace(' startswith ', '~')
+                .replace('~', 'startswith=')
+                .split(/[=]/));
+            let negate = false;
+            if (parts[0].substr(0, 4) == 'not ') {
+                negate = true;
+                parts[0] = parts[0].substr(4);
+            }
+            return ((negate ? 'not.' : '') +
+                parts[0].replace(/ /ig, '') +
+                '=' + parts[parts.length - 1].replace(/^ /, ''));
+        });
+        if (filters.length) url += '&' + filters.join('&');
+        if (self.order) url += '&@order=' + self.order;
         self.busy = true;
         axios.get(url).then(function (res) {
-                self.busy = false;
-                if(!length) self.table = res.data; 
-                else self.table.items = self.table.items.concat(res.data.items);
-            });
+            self.busy = false;
+            if (!length) self.table = res.data;
+            else self.table.items = self.table.items.concat(res.data.items);
+        });
     };
-    
+
     mtable.methods.reorder = function (field) {
         if (this.order == '~' + field.name) this.order = null;
-        else if (this.order == field.name) this.order = '~'+field.name;
+        else if (this.order == field.name) this.order = '~' + field.name;
         else this.order = field.name;
         this.table.items = [];
         this.load();
     };
 
-    mtable.methods.clear = function() {
+    mtable.methods.clear = function () {
         this.errors = {};
         this.item = null;
         this.message = '';
@@ -74,37 +76,43 @@
     mtable.methods.open_create = function () {
         this.populate_reference_options();
         this.item = {};
-        for(var field in this.model) this.item[field.name] = field.default||'';
+        for (var field of this.table.model) {
+            output = field.default != null ? field.default : '';
+            if (field.type == "datetime") {
+                output = output.split('.')[0];
+            }
+            Vue.set(this.item, field.name, output);
+        }
     };
-    
+
     mtable.methods.open_edit = function (item) {
         this.populate_reference_options();
         this.item = {};
         this.item = item;
     };
 
-    mtable.methods.populate_reference_options = function(){
+    mtable.methods.populate_reference_options = function () {
         let self = this;
-        for(var field of this.table.model){
+        for (var field of this.table.model) {
             console.log(field.name);
-            if(field.type == "reference"){
-                if (!(field.references in this.reference_options)){
+            if (field.type == "reference") {
+                if (!(field.references in this.reference_options)) {
                     Vue.set(this.reference_options, field.references, []);
-                    let reference_table_url = self.url.split('/');                    
+                    let reference_table_url = self.url.split('/');
                     reference_table_url.pop()
                     reference_table_url.push(field.references)
                     reference_table_url = reference_table_url.join('/') + '?@options_list=true';
                     axios.get(reference_table_url).then(function (res) {
                         let url_components = res.config.url.split('?')[0].split('/');
-                        self.reference_options[url_components[url_components.length - 1 ]] = res.data.items;
-                     });
-                    
+                        self.reference_options[url_components[url_components.length - 1]] = res.data.items;
+                    });
+
                 }
             }
         }
     }
 
-    mtable.methods.parse_and_validate_json = function(event){
+    mtable.methods.parse_and_validate_json = function (event) {
         try {
             event.target.style.borderColor = "";
             return JSON.parse(event.target.value);
@@ -116,29 +124,29 @@
 
     mtable.methods.trash = function (item) {
         if (window.confirm("Really delete record?")) {
-            let url = this.url + '/' + item.id;            
-            this.table.items = this.table.items.filter((i)=>{return i.id != item.id;});
+            let url = this.url + '/' + item.id;
+            this.table.items = this.table.items.filter((i) => { return i.id != item.id; });
             axios.delete(url);
-            if (item==this.item) this.item = null;
+            if (item == this.item) this.item = null;
         }
     };
-    
+
     mtable.methods.save = function (item) {
         let url = this.url;
         self.busy = true;
         if (item.id) {
             url += '/' + item.id;
             axios.put(url, item).then(mtable.handle_response('put', this),
-                                      mtable.handle_response('put', this));
+                mtable.handle_response('put', this));
         } else {
             axios.post(url, item).then(mtable.handle_response('post', this),
-                                       mtable.handle_response('post', this));
+                mtable.handle_response('post', this));
         }
     };
 
-    mtable.handle_response = function(method, data) {
+    mtable.handle_response = function (method, data) {
         self.busy = false;
-        return function(res) {
+        return function (res) {
             if (res.response) res = res.response; // deal with error weirdness
             if (method == 'post') {
                 data.table.items = [];
@@ -161,38 +169,38 @@
     mtable.methods.close = function () {
         this.clear();
     };
-    
-    mtable.methods.search = function () { 
+
+    mtable.methods.search = function () {
         this.clear();
         this.table.items = [];
         this.table.count = 0;
         this.load();
     };
 
-    mtable.methods.go_ref_by = function(tablefield, item_id) {
-        tablefield = (tablefield+'.id').split('.');
+    mtable.methods.go_ref_by = function (tablefield, item_id) {
+        tablefield = (tablefield + '.id').split('.');
         var tablename = tablefield[0], fieldname = tablefield[1];
         var source = window.location.search.substring(1);
-        var vars={}, items=source.split('&');
-        items.map(function(item){
-                var pair = item.split('=');
-                vars[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-            });
+        var vars = {}, items = source.split('&');
+        items.map(function (item) {
+            var pair = item.split('=');
+            vars[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+        });
         vars['tablename'] = tablename
-        vars['filter'] = fieldname+'=='+item_id;
-        source = Object.keys(vars).map(function(key){
-                return key+'='+encodeURIComponent(vars[key]);
-            }).join('&');
-        window.location = window.location.href.split('?')[0]+'?'+source;
+        vars['filter'] = fieldname + '==' + item_id;
+        source = Object.keys(vars).map(function (key) {
+            return key + '=' + encodeURIComponent(vars[key]);
+        }).join('&');
+        window.location = window.location.href.split('?')[0] + '?' + source;
     };
 
 
 
     var scripts = document.getElementsByTagName('script');
-    var src = scripts[scripts.length-1].src;
-    var path = src.substr(0, src.length-3) + '.html';
-    Q.register_vue_component('mtable', path, function(template) {        
-            mtable.template = template.data;
-            return mtable;
-        });
+    var src = scripts[scripts.length - 1].src;
+    var path = src.substr(0, src.length - 3) + '.html';
+    Q.register_vue_component('mtable', path, function (template) {
+        mtable.template = template.data;
+        return mtable;
+    });
 })();
