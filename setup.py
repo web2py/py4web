225a3064cef975461aa117ef8be13ce56574c012
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-08-16 05:48:09 -0700

    version 1.20210816.1

--- setup.py
+++ setup.py
@@ -23,10 +23,22 @@ setup(
     description="Experimental py4web (a better web2py)",
     packages=["py4web", "py4web.utils", "py4web.utils.auth_plugins"],
     package_data={"py4web": ["assets/*"],},
-    install_requires=[line for line in [
-        line.split("#")[0].strip()
-        for line in open("requirements.txt").readlines()        
-    ] if line],
+    install_requires=[
+        "wheel",
+        "bottle >= 0.12",
+        "click",
+        "colorama",
+        "cryptography",
+        "tornado",
+        "renoir >= 1.4.0",
+        "requests",
+        "threadsafevariable >= 1.1",
+        "pyjwt >= 2.0.1",
+        "pluralize >= 0.1.7",
+        "yatl >= 20210326.1",
+        "pydal >= 20210626.3",
+        "watchgod >= 0.6",
+    ],
     entry_points={"console_scripts": ["py4web=py4web.core:cli"],},
     zip_safe=False,
     platforms="any",
