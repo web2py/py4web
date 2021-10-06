6b7cd899f5ea1785f1a565117f2311a6ddff89dd
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-10-05 21:34:13 -0700

    version 1.20210905.1

--- py4web/__init__.py
+++ py4web/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 __author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
 __license__ = "BSDv3"
-__version__ = "1.20210816.1"
+__version__ = "1.20210905.1"
 
 
 def _maybe_gevent():
