e6c29b4b95dfab01f66c4e16b2c65f1561e61aef
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-12-27 19:08:18 -0800

    version 1.20211227.1

--- docs/chapter-16.rst
+++ docs/chapter-16.rst
@@ -7,7 +7,6 @@ Advanced topics and examples
 py4web and asyncio
 ------------------
 
-py4web (as bottle) is thread-based, with high speed and efficient memory usage.
 Asyncio is not strictly needed, at least for most of the normal use
 cases where it will add problems more than value because of its concurrency model.
 On the other hand, we think py4web needs a built-in websocket async based solution.
--- setup.py
+++ setup.py
@@ -23,23 +23,10 @@ setup(
     description="Experimental py4web (a better web2py)",
     packages=["py4web", "py4web.utils", "py4web.utils.auth_plugins"],
     package_data={"py4web": ["assets/*"],},
-    install_requires=[
-        "wheel",
-        "click",
-        "colorama",
-        "cryptography",
-        "ombott",
-        "tornado",
-        "renoir >= 1.4.0",
-        "requests",
-        "rocket3",
-        "threadsafevariable >= 1.1",
-        "pyjwt >= 2.0.1",
-        "pluralize >= 0.1.7",
-        "yatl >= 20210326.1",
-        "pydal >= 20210626.3",
-        "watchgod >= 0.6",
-    ],
+    install_requires=[line for line in [
+        line.split('#')[0].strip() for line in
+        open("requirements.txt")
+    ] if line],
     entry_points={"console_scripts": ["py4web=py4web.core:cli"],},
     zip_safe=False,
     platforms="any",
