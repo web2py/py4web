58f9643855fd67dfdc3b8b2e851476302f16817f
Massimo Di Pierro <massimo.dipierro@gmail.com>
2022-01-09 21:17:18 -0800

    myfeed->fadebook

---
+++ apps/fadebook/models.py
@@ -0,0 +1,23 @@
+from .common import *
+from pydal.validators import IS_NOT_EMPTY
+
+db.define_table(
+    "feed_item",
+    Field("body", "text", requires=IS_NOT_EMPTY()),
+    auth.signature
+)
+
+db.define_table(
+    "item_like",
+    Field("item_id", "reference feed_item"),
+    auth.signature
+)
+
+db.define_table(
+    "friend_request",
+    Field("from_user", "reference auth_user"),
+    Field("to_user", "reference auth_user"),
+    Field("status", options=("accepted", "pending")),
+)
+
+db.commit()
