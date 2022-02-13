dec9d020a45688500331aa816d6f5da28103ad15
Massimo Di Pierro <massimo.dipierro@gmail.com>
2022-02-12 21:37:31 -0800

    fixed Tag.path to tagpath, fixes #670

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "pluralize >= 0.1.9",
         "rocket3",
         "yatl >= 20211217.1",
-        "pydal >= 20220213.1",
+        "pydal >= 20220213.2",
         "watchgod >= 0.6",
     ],
     entry_points={"console_scripts": ["py4web=py4web.core:cli"],},
