5cf496661678acd7d9356e099c7ec609d3765e30
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-05-07 19:13:49 -0700

    version 1.20230507.1

--- apps/_documentation/static/pt/_static/documentation_options.js
+++ apps/_documentation/static/pt/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.20221226.1',
+    VERSION: '1.20230507.1',
     LANGUAGE: 'pt',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
