3d1c249561fa41ce6d078a3162bc67336d8ce72f
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-05-11 19:31:07 -0700

    version 1.20230511.1, upgraded pydal fixing restapi

--- py4web/__init__.py
+++ py4web/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 __author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
 __license__ = "BSDv3"
-__version__ = "1.20230507.2"
+__version__ = "1.20230511.1"
 
 
 def _maybe_gevent():
