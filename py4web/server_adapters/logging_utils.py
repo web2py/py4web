128c48d6997c18e01aa8c846e7e6d12f5e747f99
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-08-23 19:48:56 +0200

    make_logger

--- py4web/server_adapters/logging_utils.py
+++ py4web/server_adapters/logging_utils.py
@@ -3,6 +3,41 @@ import os
 import sys
 
 
+def make_logger(name, loggers_info):
+    """
+    Abstraction layer on logging. Example of usage:
+
+    from py4web.server_adapters.logging_utils import make_logger
+
+    loggers_info = [
+        "warning:warning.log",
+        "info:info.log",
+        "debug:debug.log:$(asctime)s > %(levelname)s > %(message)s",
+    ]
+
+    logger = make_logger("py4web:appname", loggers_info)
+    """
+    default_formatter = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
+    # reset loggers
+    root = logging.getLogger(name)
+    list(map(root.removeHandler, root.handlers))
+    list(map(root.removeFilter, root.filters))
+    for logger in loggers_info:
+        logger += (":stderr" if logger.count(":") == 0 else "")
+        logger += (":" if logger.count(":") == 1 else "")
+        level, filename, formatter = logger.split(":", 2)
+        if not formatter:
+            formatter = default_formatter
+        if filename in ("stdout", "stderr"):
+            handler = logging.StreamHandler(getattr(sys, filename))
+        else:
+            handler = logging.FileHandler(filename)
+        handler.setFormatter(formatter)
+        handler.setLevel(getattr(logging, level.upper(), "DEBUG"))
+        root.addHandler(handler)
+    return root
+
+
 def get_log_file(out_banner=True):
     """
     Returns the filename for logging or None
