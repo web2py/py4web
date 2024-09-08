89bc8ddb20b1bacd3803a609ac58711744d050bf
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-09-09 00:27:35 +0200

    updated the gae instructions and example

--- deployment_tools/gae/main.py
+++ deployment_tools/gae/main.py
@@ -1,11 +1,12 @@
 import os
 import site
+import uuid
 site.addsitedir(os.path.join(os.path.dirname(__file__), 'lib'))
 from py4web.core import Reloader, bottle, Session
 os.environ['PY4WEB_DASHBOARD_MODE'] = 'none'
 os.environ['PY4WEB_SERVICE_DB_URI'] = 'sqlite:memory'
 os.environ['PY4WEB_APPS_FOLDER'] = os.path.join(os.path.dirname(__file__), 'apps')
-os.environ['PY4WEB_SERVICE_FOLDER'] = os.path.join(os.path.dirname(__file__), '.service')
-Session.SECRET = open(os.path.join(os.path.dirname(__file__), '.service/session.secret'), 'rb').read()
+os.environ['PY4WEB_SERVICE_FOLDER'] = os.path.join(os.path.dirname(__file__), 'apps/.service')
+Session.SECRET = str(uuid.uuid4())
 Reloader.import_apps()
 app = bottle.default_app()
