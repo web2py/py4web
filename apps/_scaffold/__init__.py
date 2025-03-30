dc8bfe3c265123f009d3bf072c4b595c86dd6e02
Massimo Di Pierro <massimo.dipierro@gmail.com>
2025-03-29 22:40:30 -0700

    fixed check if compatible

--- apps/_scaffold/__init__.py
+++ apps/_scaffold/__init__.py
@@ -1,7 +1,7 @@
 # check compatibility
 import py4web
 
-assert py4web.check_compatible("0.1.20190709.1")
+assert py4web.check_compatible("1.20190709.1")
 
 # by importing controllers you expose the actions defined in it
 from . import controllers
