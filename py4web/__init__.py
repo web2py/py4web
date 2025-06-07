f59a17584319968c66fee459a2b2cadb44a05968
Massimo Di Pierro <massimo.dipierro@gmail.com>
2025-06-07 10:56:13 -0700

    version 1.20250607.1, upgrades pydal to 20250607.1

--- py4web/__init__.py
+++ py4web/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 __author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
 __license__ = "BSD-3-Clause"
-__version__ = "1.20250603.1"
+__version__ = "1.20250607.1"
 
 
 def _maybe_gevent():
