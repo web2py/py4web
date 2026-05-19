3a9a3e6532c8f811ab4e4c6967fce61107ff3f97
Massimo Di Pierro <massimo.dipierro@gmail.com>
2026-05-18 21:06:02 -0700

    version 1.20260518.0, upgraded yatl, pydal, dashboard, and many small fixes

--- py4web/__init__.py
+++ py4web/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 __author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
 __license__ = "BSD-3-Clause"
-__version__ = "1.20260403.2"
+__version__ = "1.20260518.0"
 
 
 def _maybe_gevent():
