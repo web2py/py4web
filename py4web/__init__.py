4611af05db79a5f2096f451836585ea62a69d744
Massimo Di Pierro <massimo.dipierro@gmail.com>
2026-05-20 19:09:46 -0700

    version 1.20260520.0, upgraded rocket3 and pydal

--- py4web/__init__.py
+++ py4web/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 __author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
 __license__ = "BSD-3-Clause"
-__version__ = "1.20260519.1"
+__version__ = "1.20260520.0"
 
 
 def _maybe_gevent():
